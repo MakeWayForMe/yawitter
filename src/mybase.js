307f68172bea54d288aed9f22dbc2e335bc4c8ec
JS_KIM <neojs89@gmail.com>
2022-06-13 22:24:01 +0900

    보안 업데이트

--- src/mybase.js
+++ src/mybase.js
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "yawitter.firebaseapp.com",
-    projectId: "yawitter",
-    storageBucket: "yawitter.appspot.com",
-    messagingSenderId: "518373226032",
-    appId: "1:518373226032:web:b35f2b1123d40020f0dff5"
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
